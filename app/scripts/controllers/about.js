9ee5e326b9606f1ac897d640710cc8ba906a0ab3
suraj.jh <suraj.jh@directi.com>
2015-07-30 14:59:29 +0530

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name roll2App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the roll2App
+ */
+angular.module('roll2App')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name roll2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the roll2App
+ */
+angular.module('roll2App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
